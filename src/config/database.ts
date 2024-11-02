8771c6640b34a5f569a4433434799a9b8adfce70
2743-123 <2743-123>
2024-11-02 14:16:36 +0530

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,11 @@ import { User } from "../entities/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "adil",
-  password: "As2743@",
-  database: "adil",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [User],
